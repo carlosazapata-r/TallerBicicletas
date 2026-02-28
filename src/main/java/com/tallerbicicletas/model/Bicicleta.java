6f328c42dfa2464e28a73a54fec9de3a912fd9bf
isabellavalenciag <isabella.valenciag@uqvirtual.edu.co>
2026-02-28 11:49:43 -0500

    Métodos Agregar servicio y obtener historial de servicios a la clase Bicicleta

--- src/main/java/com/tallerbicicletas/model/Bicicleta.java
+++ src/main/java/com/tallerbicicletas/model/Bicicleta.java
@@ -1,32 +1,40 @@
 package com.tallerbicicletas.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bicicleta {
 
     private String color;
     private String marca;
     private String nombreSerie;
     private TipoBicicleta tipo;
+    private List<Servicio> listaServicios;
+    private String año;
+    private Taller taller;
+    private Cliente cliente;
 
-    public Bicicleta(String color, String marca, String nombreSerie, TipoBicicleta tipo) {
+    public Bicicleta(String color, String marca, String nombreSerie, TipoBicicleta tipo,String año,Taller taller,Cliente cliente) {
         this.color = color;
         this.marca = marca;
         this.nombreSerie = nombreSerie;
         this.tipo = tipo;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getMarca() {
-        return marca;
+        this.listaServicios = new ArrayList<>();
+        this.año = año;
+        this.cliente= cliente;
+        this.taller = taller;
     }
 
     public String getNombreSerie() {
         return nombreSerie;
     }
 
-    public TipoBicicleta getTipo() {
-        return tipo;
+    public void agregarServicio(Servicio servicio) {
+        if(servicio != null){
+            listaServicios.add(servicio);
+        }
+    }
+    public List<Servicio> obtenerHistorialServicios(){
+        return listaServicios;
     }
 }
