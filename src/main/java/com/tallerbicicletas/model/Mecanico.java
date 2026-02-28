39cd134cad39a27a6d8d47ee2c7875fc795dfb11
isabellavalenciag <isabella.valenciag@uqvirtual.edu.co>
2026-02-28 15:53:47 -0500

    Funcionalidad propia

--- src/main/java/com/tallerbicicletas/model/Mecanico.java
+++ src/main/java/com/tallerbicicletas/model/Mecanico.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 public class Mecanico extends Persona {
     private List<Servicio> listaServicios;
+    private boolean ocupado;
     private TipoEspecialidad tipoEspecialidad;
 
     public Mecanico(String id, String nombre,Taller taller,TipoEspecialidad tipoEspecialidad) {
         super(id, nombre,taller);
         this.listaServicios=new ArrayList<>();
         this.tipoEspecialidad = tipoEspecialidad;
+        this.ocupado = false;
+    }
+    public boolean estaDisponible(){
+        return !ocupado;
+    }
+    public void cambiarEstado(boolean estado){
+        this.ocupado = estado;
     }
 }
